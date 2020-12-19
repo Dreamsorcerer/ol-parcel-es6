e12eb357ab2c78eb6d68004a7d13326fd9bc92ad
Sam Bull <aa6bs0@sambull.org>
2020-12-19 16:56:46 +0000

    Alternative syntax to avoid default export

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 export {Map, View} from 'ol';
-export {default as TileLayer} from 'ol/layer/Tile';
-export {default as OSM} from 'ol/source/OSM';
+export {Tile as TileLayer} from 'ol/layer';
+export {OSM} from 'ol/source';
